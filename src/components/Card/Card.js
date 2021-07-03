3feeaae2d889f1ca6b071f0d2dae872de9e8b88d
Ravil <ravil377@mail.ru>
2021-07-03 18:06:31 +0500

    fix scroll

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,22 +3,15 @@ import { CurrentMovieContext } from '../../contexts/CurrentMovieContext';
 import CardDetails from '../cardDetails/cardDetails';
 
 function Card(props) {
-    const [isOpenDetails, setIsOpenDetails] = React.useState(false);
     const isMovieDetails = React.useContext(CurrentMovieContext);
 
-    
     const handleClickDetails = () => {
         if(props.movie.imdbID !== isMovieDetails.imdbID) {
             props.onClick(props.movie.imdbID);
-            setIsOpenDetails(true);
         } else {
             props.onClick(null);
-            setIsOpenDetails(false);
         }
-
-        
     }
-    
 
     return (
         <li className="card">
@@ -27,7 +20,7 @@ function Card(props) {
                 <figcaption className="card__caption">{props.movie.Title}</figcaption>
             </figure>
             <p className="card__more" onClick={handleClickDetails}>Показать детали</p>
-            <div className={`card__details ${(props.movie.imdbID === isMovieDetails.imdbID) && 'card__details_open'}`}>
+            <div className={`card__details ${props.movie.imdbID === isMovieDetails.imdbID && 'card__details_open'}`}>
                 <CardDetails />
             </div>
         </li>
